2c3ff892e3a7a97f7af99dcb1ae02abad9c1b715
Pannnomaly <supawith007@gmail.com>
2026-08-06 00:34:02 +0700

    fix: update skills in service section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -10,7 +10,7 @@ export default function Services ()
         <div className="pt-20 xl:min-h-screen md:pt-36 md:pb-16">
             <Container>
                 <motion.h2 {...servicesTitleReveal} className="text-center text-[2rem] md:text-[2.5rem] lg:text-[3rem] xl:text-[3.25rem]">
-                    What I <span className="text-(--first-color)">Offer</span>
+                    What I <span className="text-(--first-color)">Can Do</span>
                 </motion.h2>
 
                 <motion.div {...servicesCardsReveal} id="service-card" className="flex flex-col lg:grid lg:items-start gap-y-4 mt-16 mb-10 lg:mt-20 lg:mb-14 xl:mt-22 xl:mb-18">
