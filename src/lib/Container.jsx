c639628bc99e104b58d927866eef98d94366fed3
Pannnomaly <supawith007@gmail.com>
2026-09-04 00:52:43 +0700

    feat(ui): redesign portfolio landing page

--- src/lib/Container.jsx
+++
@@ -1,8 +0,0 @@
-export default function Container ({ children, className = "" })
-{
-    return (
-        <div className={`max-w-400 mx-auto px-6 ${className}`}>
-            {children}
-        </div>
-    );
-}
